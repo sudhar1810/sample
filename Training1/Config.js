abf37b809aa9671c52082a4bd77bb4c31e47ec4b
resh226 <reshmasajeevpulickal@gmail.com>
2020-04-02 09:51:15 +0530

    secondchanges

--- Training1/Config.js
+++ Training1/Config.js
@@ -2,7 +2,7 @@ exports.config = {
     framework: 'jasmine',
     directConnect : "true",
     //seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['one.js'],
+    specs: ['LaunchGoogle.js'],
     capabilities: {
       browserName: 'chrome'
     }
